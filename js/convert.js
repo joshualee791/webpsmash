c2e08c29513927fdb75b1ee0c8ba9d78aca59b8c
Joshua <120612998+joshualee791@users.noreply.github.com>
2023-03-14 00:04:36 -0500

    debug2

--- js/convert.js
+++ js/convert.js
@@ -1,28 +1,30 @@
-function convertWebP() {
-  const input = document.querySelector('input[type="file"]');
+function convertFile() {
+  const input = document.getElementById("fileToConvert");
   const file = input.files[0];
-  if (file.type === "image/webp") {
+  if (file) {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => {
+      const dataUrl = reader.result;
       const img = new Image();
-      img.src = reader.result;
+      img.src = dataUrl;
       img.onload = () => {
         const canvas = document.createElement("canvas");
         canvas.width = img.width;
         canvas.height = img.height;
         const ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-        const dataURL = canvas.toDataURL("image/png");
-        const downloadLink = document.createElement("a");
-        downloadLink.href = dataURL;
-        downloadLink.download = "converted.png";
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        canvas.toBlob((blob) => {
+          const downloadLink = document.createElement("a");
+          downloadLink.href = URL.createObjectURL(blob);
+          downloadLink.download = file.name.replace(/\.[^/.]+$/, "") + ".png";
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          document.body.removeChild(downloadLink);
+        }, "image/png");
       };
     };
   } else {
-    alert("Please select a WebP file to convert.");
+    alert("Please select a file to convert.");
   }
 }
